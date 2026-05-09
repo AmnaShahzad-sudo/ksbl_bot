fc4a9a0dbc103c7ab98f2bd8d11162a46ea7afd8
AmnaShahzad-sudo <amnashahzad840@gmail.com>
2026-05-09 22:31:11 +0500

    Update mvp.py

--- mvp.py
+++ mvp.py
@@ -10,7 +10,7 @@ import numpy as np
 import streamlit as st
 import voyageai
 
-st.set_page_config(page_title="KSBLBot", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="KSBLBot", initial_sidebar_state="collapsed" if not st.query_params.get("admin") else "expanded")
 
 ADMIN_KEYS = ['adminmodeenable']
 
