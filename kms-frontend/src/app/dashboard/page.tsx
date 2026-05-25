bc816e120492afebdd24bfc4c35d3341ecf967b6
Abdullah-Dojky <b040007aw@ksbl.edu.pk>
2026-05-25 14:35:04 +0500

    Updated Backend URL

--- kms-frontend/src/app/dashboard/page.tsx
+++ kms-frontend/src/app/dashboard/page.tsx
@@ -17,7 +17,7 @@ import {
   AlertCircle
 } from "lucide-react";
 
-const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://ksbl-bot.onrender.com/v1";
+const API_BASE =  "https://ksbl-bot.onrender.com/v1";
 
 interface FileData {
   filename: string;
